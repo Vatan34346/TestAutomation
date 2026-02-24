3c631c5883d084190fd0ad89b388afc28f1b1bbb
Vatan34346 <paatagvichia@gmail.com>
2026-02-24 18:44:43 +0400

    fix evirenmetn error with USRER AND PASSWORD SECRETS

--- core/config.py
+++ core/config.py
@@ -3,6 +3,6 @@ import os
 
 class Settings:
     BASE_URL: str | None = os.getenv("BASE_URL")  # "https://api.casino-stage.com/api/v1/"
-    USERNAME: str | None = os.getenv("archangel")  # "archangel"
-    PASSWORD: str | None = os.getenv("Mike2025")  # "Mike2025!"
+    USERNAME: str | None = os.getenv("USERNAME")  # "archangel"
+    PASSWORD: str | None = os.getenv("PASSWORD")  # "Mike2025!"
 
